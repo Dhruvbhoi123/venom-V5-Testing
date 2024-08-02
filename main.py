176b1aa74eeebb44e54b704cca2514982d957049
Dhruvbhoi123 <dhruvbhoi123456@gmail.com>
2024-08-03 00:18:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -60,8 +60,8 @@ def write_config(config):
 # SCRIPT MADE BY @venomXcrazy ON TELEGRAM THIS IS A PAID SCRIPT MOST POWERFULL A DDOS TELEGRAM BOT SCRIPT
 
 config = load_config()
-bot = telebot.TeleBot(config['bot_token'])
-ADMIN_IDS = set(config['admin_ids'])
+bot = telebot.TeleBot(config['7278104058:AAF4YbePBr_VVa73pFvecw4wNKW0RIkmv_U'])
+ADMIN_IDS = set(config['1495897069'])
 USER_FILE = config['user_file']
 LOG_FILE = config['log_file']
 COOLDOWN_TIME = config['cooldown_time']
